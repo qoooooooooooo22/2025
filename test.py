6c90af6c61d6492a5eb65c66926530898ee8d2b1
qoooooooooooo22 <chyeon090@gmail.com>
2025-08-24 09:59:01 +0900

    Update test.py

--- test.py
+++ test.py
@@ -3,45 +3,36 @@ import requests
 import random
 
 # --- 페이지 설정 ---
-st.set_page_config(page_title="감정 음악 추천기 (무료+무작위)", page_icon="🎵")
+st.set_page_config(page_title="감정 음악 추천기 (30곡 무작위)", page_icon="🎵")
 
 # --- YouTube API 키 ---
 YOUTUBE_API_KEY = "AIzaSyBLuzIZRaRKshJJkGClpLDrPB55F0ETfVo"
 
-# --- 감정별 노래 리스트 (장르 다양화) ---
+# --- 감정별 30곡 리스트 예시 (다양한 장르 포함) ---
 emotion_songs = {
     "사랑": [
         "All of Me John Legend", "Just the Way You Are Bruno Mars", "Fly Me to the Moon Frank Sinatra",
-        "Lover Taylor Swift", "Your Song Elton John", "At Last Etta James"
+        "Lover Taylor Swift", "Your Song Elton John", "At Last Etta James",
+        "Perfect Ed Sheeran", "Can't Help Falling in Love Elvis Presley", "Endless Love Diana Ross & Lionel Richie",
+        "Make You Feel My Love Adele", "Something The Beatles", "When I Fall in Love Nat King Cole",
+        "My Heart Will Go On Celine Dion", "I Will Always Love You Whitney Houston", "Love Story Taylor Swift",
+        "Thinking Out Loud Ed Sheeran", "How Deep Is Your Love Bee Gees", "Unchained Melody The Righteous Brothers",
+        "All My Life K-Ci & JoJo", "Because You Loved Me Celine Dion", "A Thousand Years Christina Perri",
+        "Vision of Love Mariah Carey", "Everything Michael Bublé", "I Just Called to Say I Love You Stevie Wonder",
+        "Truly Madly Deeply Savage Garden", "Bleeding Love Leona Lewis", "Time After Time Cyndi Lauper",
+        "Endless Love Luther Vandross", "Kiss Me Sixpence None the Richer"
     ],
-    "이별": [
-        "Someone Like You Adele", "Back to December Taylor Swift", "Ne Me Quitte Pas Jacques Brel",
-        "Un-break My Heart Toni Braxton", "Tears Dry on Their Own Amy Winehouse"
-    ],
-    "집착": [
-        "Every Breath You Take The Police", "Obsessed Mariah Carey", "Creep Radiohead"
-    ],
-    "행복": [
-        "Happy Pharrell Williams", "Walking on Sunshine Katrina & The Waves", "Can't Stop the Feeling Justin Timberlake"
-    ],
-    "귀여움": [
-        "Sugar Maroon 5", "Call Me Maybe Carly Rae Jepsen", "Shake It Off Taylor Swift"
-    ],
-    "우정": [
-        "Lean On Me Bill Withers", "Count on Me Bruno Mars", "With a Little Help From My Friends The Beatles"
-    ],
-    "위로": [
-        "Fix You Coldplay", "Stand By Me Ben E. King", "Hallelujah Jeff Buckley"
-    ],
-    "추억": [
-        "Yesterday The Beatles", "Summer of '69 Bryan Adams", "Viva La Vida Coldplay"
-    ],
-    "그리움": [
-        "I Will Remember You Sarah McLachlan", "Somewhere I Belong Linkin Park", "Photograph Ed Sheeran"
-    ],
-    "슬픔": [
-        "Mad World Gary Jules", "The Sound of Silence Simon & Garfunkel", "Everybody Hurts R.E.M."
-    ]
+    # 다른 감정도 비슷하게 30곡 정도 추가
+    "이별": ["Someone Like You Adele", "Back to December Taylor Swift", "Ne Me Quitte Pas Jacques Brel",
+            "Un-break My Heart Toni Braxton", "Tears Dry on Their Own Amy Winehouse"] * 6,  # 임시 반복
+    "집착": ["Every Breath You Take The Police", "Obsessed Mariah Carey", "Creep Radiohead"] * 10,
+    "행복": ["Happy Pharrell Williams", "Walking on Sunshine Katrina & The Waves", "Can't Stop the Feeling Justin Timberlake"] * 10,
+    "귀여움": ["Sugar Maroon 5", "Call Me Maybe Carly Rae Jepsen", "Shake It Off Taylor Swift"] * 10,
+    "우정": ["Lean On Me Bill Withers", "Count on Me Bruno Mars", "With a Little Help From My Friends The Beatles"] * 10,
+    "위로": ["Fix You Coldplay", "Stand By Me Ben E. King", "Hallelujah Jeff Buckley"] * 10,
+    "추억": ["Yesterday The Beatles", "Summer of '69 Bryan Adams', 'Viva La Vida Coldplay"] * 10,
+    "그리움": ["I Will Remember You Sarah McLachlan", "Somewhere I Belong Linkin Park", "Photograph Ed Sheeran"] * 10,
+    "슬픔": ["Mad World Gary Jules", "The Sound of Silence Simon & Garfunkel", "Everybody Hurts R.E.M."] * 10
 }
 
 emoji_map = {
@@ -56,7 +47,7 @@ def search_youtube_video(api_key, query):
         "part": "snippet",
         "q": query,
         "type": "video",
-        "maxResults": 5,  # 여러 영상 가져오기
+        "maxResults": 5,
         "key": api_key
     }
     resp = requests.get(url, params=params)
@@ -67,12 +58,12 @@ def search_youtube_video(api_key, query):
             title = vid["snippet"]["title"]
             thumb = vid["snippet"]["thumbnails"]["high"]["url"]
             url = f"https://www.youtube.com/watch?v={vid_id}"
-            if query.split()[0].lower() in title.lower():  # 검색어 일부 포함
+            if query.split()[0].lower() in title.lower():
                 return {"title": title, "url": url, "thumbnail": thumb}
     return None
 
 # --- UI ---
-st.title("🎶 감정 키워드 기반 음악 추천기 (무료+무작위)")
+st.title("🎶 감정 키워드 기반 음악 추천기 (30곡 무작위)")
 
 selected_emotion = st.selectbox("오늘 당신의 감정은?", list(emotion_songs.keys()))
 emoji = emoji_map.get(selected_emotion, "")
@@ -81,7 +72,7 @@ if st.button("🎧 추천곡 보기"):
     st.markdown(f"## {emoji} {selected_emotion} 감정에 어울리는 노래들")
 
     # --- 무작위 추천: 감정 리스트에서 3곡 선택 ---
-    songs = random.sample(emotion_songs[selected_emotion], k=min(3, len(emotion_songs[selected_emotion])))
+    songs = random.sample(emotion_songs[selected_emotion], k=3)
 
     for song in songs:
         yt = search_youtube_video(YOUTUBE_API_KEY, song)
